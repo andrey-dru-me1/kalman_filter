c29c60b1681b0c9cd271292637895ab9baa05f2d
Andrey Melnikov <a.melnikov4@g.nsu.ru>
2025-12-14 18:07:51 +0700

    Add base for planes

--- src/main.py
+++ src/main.py
@@ -43,6 +43,44 @@ class SpringWeightModel(KalmanModel):
         self.z_clean = z_clean
 
 
+class F16Model(KalmanModel):
+    n = 1
+    nx = 2
+    nz = 2
+    nu = 1
+    F = np.array([[-0.6, 0.95], [-4.3, -1.2]])
+    G = np.array([[-0.115], [-5.157]])
+    H = np.array([[1, 0], [0, 1]])
+    w = np.array([[0]])
+    v = np.array([[5]])
+
+    def __init__(self, size: int, f: Callable):
+        F = self.F
+        G = self.G
+        H = self.H
+        w = self.w
+        v = self.v
+
+        N = int(size / self.n)
+        abscissa = np.linspace(0, size, N)
+        u = np.array([[[f(x)]] for x in abscissa])
+
+        x = np.zeros((N, self.nx, 1))
+
+        z = np.empty((N, self.nz, 1))
+        z_clean = np.empty((N, self.nz, 1))
+        for i in range(1, N):
+            x[i] = F @ x[i - 1] + G @ u[i - 1] + w
+            z_clean[i] = H @ x[i]
+            z[i] = H @ x[i] + np.random.normal(0, v)
+
+        self.x = x
+        self.z = z
+        self.u = u
+        self.abscissa = abscissa
+        self.z_clean = z_clean
+
+
 if __name__ == "__main__":
     spring_weight_model = SpringWeightModel(400, lambda x: np.sign(np.sin(0.05 * x)))
     prediction = KalmanFilter.filter(
